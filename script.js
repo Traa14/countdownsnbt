664769af0e34c342cd4fb239e87ad4ab67e9a053
Traa <121452291+Traa14@users.noreply.github.com>
2026-04-12 12:56:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateCountdown(){
 
 const now = new Date().getTime();
-const targetDate = new Date("Apr 21, 2026 05:59:59").getTime();
+const targetDate = new Date("Apr 29, 2026 23:59:59").getTime();
 const distance = targetDate - now;
 
 if(distance <= 0){
