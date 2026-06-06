fa9541c6815816363714a97a5690ad4d4d0b6cab
Traa <121452291+Traa14@users.noreply.github.com>
2026-06-06 09:33:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateCountdown(){
 
 const now = new Date().getTime();
-const targetDate = new Date("May 25, 2026 14:59:59").getTime();
+const targetDate = new Date("June 12, 2026 14:59:59").getTime();
 const distance = targetDate - now;
 
 if(distance <= 0){
